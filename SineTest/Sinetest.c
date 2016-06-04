0a4ff13f9c727be7c5172576a562a09b85a9d839
KamilPiekutowski <kpieku2@uic.edu>
2016-06-03 22:26:33 -0500

    success in testing envelope-like behavior

---
+++ SineTest/Sinetest.c
@@ -0,0 +1,78 @@
+#include <alsa/asoundlib.h>
+#include <alsa/pcm.h>
+#include <math.h>
+#define BUFFER_LEN 60000
+
+static char *device = "default";                       //soundcard
+snd_output_t *output = NULL;
+float buffer [BUFFER_LEN];
+
+
+int main(void)
+{
+    int err;
+    int j,k;
+
+    int f = 440;                //frequency
+    int fs = 48000;             //sampling frequency
+
+    snd_pcm_t *handle;
+    snd_pcm_sframes_t frames;
+
+
+    // ERROR HANDLING
+
+    if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+            printf("Playback open error: %s\n", snd_strerror(err));
+            exit(EXIT_FAILURE);
+    }
+
+    if ((err = snd_pcm_set_params(handle,
+                                  SND_PCM_FORMAT_FLOAT,
+                                  SND_PCM_ACCESS_RW_INTERLEAVED,
+                                  1,
+                                  48000,
+                                  1,
+                                  500000)) < 0) {
+            printf("Playback open error: %s\n", snd_strerror(err));
+            exit(EXIT_FAILURE);
+
+
+    }
+
+    // SINE WAVE
+    printf("\nSine tone at %dHz\n",f);
+  //  int i = 10;
+  //  while(i > 1){
+        float amp = 1;
+        for (k=0; k<BUFFER_LEN; k++){
+
+            buffer[k] = (sin(2*M_PI*f/fs*k) * amp);
+            amp -= .00005;
+            if(amp < 0){
+                amp = 0;
+            }             //sine wave value generation
+            printf("AMP: %f",amp);
+
+            }
+
+       // for (j=0; j<5; j++){
+            frames = snd_pcm_writei(handle, buffer, BUFFER_LEN);    //sending values to sound driver
+       //     }
+          //  i--;
+            //printf("i: %d\n",i);
+   //  }
+   for (k=0; k<BUFFER_LEN; k++){
+
+            buffer[k] = (sin(2*M_PI*f*2/fs*k));                 //sine wave value generation
+            }
+
+       // for (j=0; j<5; j++){
+            frames = snd_pcm_writei(handle, buffer, BUFFER_LEN);
+
+
+
+    snd_pcm_close(handle);
+    return 0;
+
+}
